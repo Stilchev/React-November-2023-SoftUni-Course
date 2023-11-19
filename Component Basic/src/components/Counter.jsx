a44282bbe1c244de417755a525bf1ef9f5af17d6
Stilchev <114847091+Stilchev@users.noreply.github.com>
2023-11-19 12:20:08 +0200

    Conditional rendering
    Conditional rendering demo

--- Component Basic/src/components/Counter.jsx
+++ Component Basic/src/components/Counter.jsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 export default function Counter(props) {
     const [count, setCount] = useState(0);
 
-    const incrementCountHandler = ()=>{
+    const incrementCountHandler = () => {
         setCount(a => a + 1)
     }
 
@@ -11,11 +11,64 @@ export default function Counter(props) {
         setCount(0)
     }
 
+    // if(count < 0) {
+    //     return(
+    //         <h5>Invalid count</h5>
+    //     )
+    // }
+
+    let message = null;
+
+    switch (count) {
+        case 1:
+            message = 'Vex rune'
+
+            break;
+        case 2:
+            message = 'Ohm rune'
+
+            break;
+            case 3:
+            message = 'Lo rune'
+            
+            break;
+            case 4:
+            message = 'Sur rune'
+            
+            break;
+            case 5:
+            message = 'Ber rune'
+            
+            break;
+            case 6:
+            message = 'Jah rune'
+            
+            break;
+
+            case 7:
+            message = 'Cham rune'
+            
+            break;
+            case 8:
+            message = 'Zod rune'
+            
+            break;
+
+        default:
+            message = 'Not a high rune'
+            break;
+    }
+
     return (
         <div>
             <h1>Counter</h1>
 
+            {count < 0 ? <p>Invalid count!</p> : <p>Valid count!</p>}
+
+            {count == 0 && <p>Please start rune hunt</p>}
+
             <p>Count is: {count}</p>
+            <h3>{message}</h3>
 
             <button onClick={() => setCount(count - 1)}>-</button>
             <button onClick={clearCountHandler}>Clear</button>
