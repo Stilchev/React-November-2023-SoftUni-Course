a25e1c1ddce31415d2583c9c2b21dab228148b89
Stilchev <114847091+Stilchev@users.noreply.github.com>
2023-11-20 08:54:54 +0200

    Components Deep Dive useEffect
    useEffect Demo

---
+++ Components Deep Dive/src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+
+import './App.css'
+
+function App() {
+    const [numbers, setNumbers] = useState([1, 2, 3, 4, 5])
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        console.log('Mount component');
+    }
+        , []);
+
+
+    useEffect(() => {
+        console.log(`Update components - ${numbers.length}`);
+    }, [numbers]);
+
+    const onClick = () => {
+        setNumbers(oldState => oldState.slice(0, oldState.length - 1))
+    }
+
+    // useEffect(() => {
+    //     setTimeout(() => setCount(c => c + 1), 1000);
+    // },[count])
+
+
+
+    return (
+        <div>
+            <h3>Count: {count}</h3>
+
+            <ul>
+                {numbers.map((number, index) => <li data-key={index} key={index}> {number * 2}</li>)}
+            </ul>
+            <button onClick={onClick}>Remove</button>
+            <button onClick={() => setCount(c => c + 1)}>+</button>
+
+        </div>
+    )
+}
+
+export default App
