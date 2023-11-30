6f18463f08e74da1ee23a55aee83634df490c335
Stilchev <114847091+Stilchev@users.noreply.github.com>
2023-11-30 14:55:21 +0200

    Save new comments

--- Games Play/client/src/components/details/Details.jsx
+++ Games Play/client/src/components/details/Details.jsx
@@ -6,11 +6,15 @@ import * as commentService from "../../services/commentService"
 
 export default function Details() {
     const [game, setGame] = useState({})
+    const [comments, setComments] = useState([])
     const { gameId } = useParams()
 
     useEffect(() => {
         gameService.getOne(gameId)
-            .then(setGame)
+            .then(setGame);
+
+        commentService.getAll()
+            .then(setComments);
     }, [gameId]);
 
     const addCommentHandler = async (e) => {
@@ -41,15 +45,16 @@ export default function Details() {
                     <h2>Comments:</h2>
                     <ul>
                         {/* list all comments for current game (If any) */}
-                        <li className="comment">
-                            <p>Content: I rate this one quite highly.</p>
-                        </li>
-                        <li className="comment">
-                            <p>Content: The best game.</p>
-                        </li>
+                        {comments.map(comment => (
+                            <li className="comment" key={comment._id}>
+                                <p>{comment.username}: {comment.text}</p>
+                            </li>
+                        ))}
+
                     </ul>
                     {/* Display paragraph: If there are no games in the database */}
-                    <p className="no-comment">No comments.</p>
+                    {comments.length === 0 && <p className="no-comment">No comments.</p>}
+                    
                 </div>
                 {/* Edit/Delete buttons ( Only for creator of this game )  */}
                 {/* <div className="buttons">
